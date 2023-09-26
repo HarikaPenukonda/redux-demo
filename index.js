e83f541f14d6ee2b13c7d175be1babb8e970e9e1
HarikaPenukonda <penukondaharika@gmail.com>
2023-09-26 17:49:05 -0500

    2. Allows access to state via getState()

--- index.js
+++ index.js
@@ -34,3 +34,5 @@ const reducer = (previousState = initialState, action) =>{
 }
 // Holds application state
 const store = createStore(reducer)
+// Allows access to state via getState()
+console.log("Initial State", store.getState())
